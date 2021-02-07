6bbf2ab1748e494bfbcf1390da8acd197f04710b
p.kupniewski <pkupniewski.dev@gmail.com>
2021-02-07 11:48:02 +0100

    simulation check

--- FactoryMethod/src/main/java/Simulator.java
+++ FactoryMethod/src/main/java/Simulator.java
@@ -1,36 +1,38 @@
-import factoryMethod.BikeRouteFinder;
-import factoryMethod.CarRouteFinder;
-import factoryMethod.RouteFactory;
-import factoryMethod.RouteFinder;
 import factorymethod2.Source;
+import factorymethod2.SourceFactory;
+import uglyjava.BikeRouteFinder;
+import uglyjava.CarRouteFinder;
 
 public class Simulator {
 
     public static void main(String[] args) {
 
         //without design pattern
-        RouteFinder bikeRouteFinder = new BikeRouteFinder();
+        uglyjava.RouteFinder bikeRouteFinder = new BikeRouteFinder();
         bikeRouteFinder.findDestinationRoute();
 
         CarRouteFinder carRouteFinder = new CarRouteFinder();
         carRouteFinder.findDestinationRoute();
 
+        /*
+            withing the factory, we have encapsulated object constructors, so we cannot create class strictly by the 'new' keyword - package private object classes
+            developers knows from the beginning they should create object, but they gave no impact on the creation process, so they are able to screw up less
+        */
 
-        //with desing patter
-        RouteFactory routeFactory = new RouteFactory();
-        RouteFinder bikeRouteFinderWithFactory = routeFactory.createRouteFinder("bike");
-        bikeRouteFinderWithFactory.findDestinationRoute();
+        //implementations after Tomeks advieses
 
-        RouteFinder carRouteFinderWithFactory = routeFactory.createRouteFinder("car");
-        carRouteFinderWithFactory.findDestinationRoute();
+        SourceFactory factory = new SourceFactory();
+        Source cinemaSource = factory.createSourceByType("cinema");
+        Source roadSource = factory.createSourceByType("road");
 
+        Source cinemaSource2 = factory.createCinemaSource();
+        Source roadSource2 = factory.createRoadSource();
 
-        // with design pattern 2
-        Source routeSource = Source.createRoadSource();
-        Source cinemasSource = Source.createCinemasSource();
+        System.out.println("Cinema  " + cinemaSource.printWhoAmI());
+        System.out.println("Road  " + roadSource.printWhoAmI());
 
-        System.out.println("route source " + routeSource);
-        System.out.println("route cinemasSource " + cinemasSource);
+        System.out.println("Cinema2  " + cinemaSource2.printWhoAmI());
+        System.out.println("Road2  " + roadSource2.printWhoAmI());
 
     }
 }
