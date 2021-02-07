81aa39b320094fcff49935445b371c8a5bace47d
p.kupniewski <pkupniewski.dev@gmail.com>
2021-02-07 11:47:51 +0100

    added  factory source

--- FactoryMethod/src/main/java/factorymethod2/SourceFactory.java
+++ FactoryMethod/src/main/java/factorymethod2/SourceFactory.java
@@ -1,2 +1,21 @@
-package factorymethod2;public class SourceFactory {
+package factorymethod2;
+
+
+public class SourceFactory {
+    public Source createSourceByType(String type) {
+        if (type.equalsIgnoreCase("cinema")) {
+            return new CinemasSource();
+        } else {
+            return new RoadSource();
+        }
+    }
+
+    //here is also possible to do ii , like that ,  this approach is cleaner for me
+
+    public Source createRoadSource(){
+        return new RoadSource();
+    }
+    public Source createCinemaSource(){
+        return new CinemasSource();
+    }
 }
