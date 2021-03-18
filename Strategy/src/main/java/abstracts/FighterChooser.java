d0635cb44a44c4ea1febebfd73f288062f0a2521
p.kupniewski <p.kupniewski@sportradar.com>
2021-03-18 09:09:49 +0100

    ADD :factory fighter

--- Strategy/src/main/java/abstracts/FighterChooser.java
+++ Strategy/src/main/java/abstracts/FighterChooser.java
@@ -1,8 +1,12 @@
-import io.vavr.API;
+package abstracts;
+
 import io.vavr.Tuple2;
 
+import static io.vavr.API.*;
+
 public class FighterChooser {
-    public Tuple2<Fighter,Fighter> choose() {
-        return new API.Tuple(new Fighter("Kasza"), new Fighter("Wasza"));
+
+    public Tuple2<Fighter, Fighter> choose() {
+        return Tuple(new Fighter("Anrjew", 1.0d, new BoxerPunch(), new BoxerKick()), new Fighter("Vandal", 1.0d, new BoxerPunch(), new BoxerKick()));
     }
 }
