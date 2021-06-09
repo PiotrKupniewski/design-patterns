1591914b114b212ab0441757da5cc8dfe51467af
p.kupniewski <=>
2021-06-09 21:31:48 +0200

    Initial tasks added

---
+++ StrategyImpl/src/main/java/Simulation.java
@@ -0,0 +1,9 @@
+public class Simulation {
+  public static void main(String[] args) {
+    //zczytywanie etykiety
+    //zczytywanie worków
+    //sortowanie worków
+    //pakowanie worków
+
+  }
+}
