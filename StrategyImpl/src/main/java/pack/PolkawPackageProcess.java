e9f904afe976f326d4e89c95f5310d553d9a8124
p.kupniewski <=>
2021-07-01 23:04:36 +0200

    Added package process

---
+++ StrategyImpl/src/main/java/pack/PolkawPackageProcess.java
@@ -0,0 +1,12 @@
+package pack;
+
+import sort.SortedCoffeeBag;
+
+import java.util.List;
+
+public class PolkawPackageProcess implements PackageProcess {
+  @Override
+  public List<PackagedCoffeeBag> packCoffee(List<SortedCoffeeBag> sortedBags) {
+    return null;
+  }
+}
