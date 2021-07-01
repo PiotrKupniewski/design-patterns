b94d218439df83677b0e91692a94845ecc87e9c5
p.kupniewski <=>
2021-07-01 23:03:40 +0200

    Added new test for polkaw

--- StrategyImpl/src/test/java/SimulationTest.java
+++ StrategyImpl/src/test/java/SimulationTest.java
@@ -1,12 +1,11 @@
 import org.junit.jupiter.api.Test;
-import pack.BrazilianPackageProcess;
-import pack.PackageManager;
+import pack.PackageProcess;
+import pack.PackageProcessFactory;
 import pack.PackagedCoffeeBag;
 import scan.CoffeeBag;
 import scan.Label;
 import scan.LabelScanner;
 import scan.Truck;
-import sort.CoffeeSorter;
 import sort.SortingProcessFactory;
 
 import java.util.List;
@@ -16,54 +15,72 @@ import static org.junit.jupiter.api.Assertions.*;
 public class SimulationTest {
 
   @Test
-  public void simulationTest(){
+  public void polkawTest(){
 
+    //given
+    List<CoffeeBag> polkawBags = List.of(
+        new CoffeeBag(new Label("B1")),
+        new CoffeeBag(new Label("")),
+        new CoffeeBag(new Label("452")),
+        new CoffeeBag(new Label("555")),
+        new CoffeeBag(new Label("2")),
+        new CoffeeBag(new Label("1"))
+    );
+
+    Label polkawLabel = new Label("Polkaw");
+
+    //when
+    List<PackagedCoffeeBag> packagedCoffeeBags = runProcess(polkawBags, polkawLabel);
+
+    //excpected
+    PackagedCoffeeBag wyborna2kg = new PackagedCoffeeBag(CoffeeBag.CoffeeQuality.WYBORNA, 2.0);
+    PackagedCoffeeBag dobra1kg = new PackagedCoffeeBag(CoffeeBag.CoffeeQuality.DOBRA, 1.0);
+    PackagedCoffeeBag swietna1kg = new PackagedCoffeeBag(CoffeeBag.CoffeeQuality.ŚWIETNA, 1.0);
+
+    //then
+    assertEquals(wyborna2kg, packagedCoffeeBags.get(5));
+    assertEquals(swietna1kg, packagedCoffeeBags.get(3));
+    assertEquals(dobra1kg, packagedCoffeeBags.get(2));
+  }
+
+
+  @Test
+  public void simulationBrazilianTest(){
+
+    //given
     List<CoffeeBag> bags = List.of(new CoffeeBag(new Label("B1")),
         new CoffeeBag(new Label("")),
         new CoffeeBag(new Label("A452")),
         new CoffeeBag(new Label("A555")),
         new CoffeeBag(new Label("B2")),
         new CoffeeBag(new Label("C1")),
-        new CoffeeBag(new Label("C1232")));
+        new CoffeeBag(new Label("C1232"))
+    );
 
-    Label polkawLabel = new Label("Polkaw");
     Label brazilianaLabel = new Label("Brazilian");
-    Truck truck = new Truck(brazilianaLabel,bags);
 
+    //when
+    List<PackagedCoffeeBag> packagedCoffeeBags = runProcess(bags, brazilianaLabel);
 
-    // read truck label
-    LabelScanner labelScanner = new LabelScanner();
-    String scannedLabel = labelScanner.scan(truck);
-
-    assertNotNull(scannedLabel);
-    assertEquals("Brazilian", scannedLabel);
-
-    //sort coffee based on labels
-    var sortingProcessFactory = new SortingProcessFactory();
-
-    CoffeeSorter coffeeSorter = new CoffeeSorter(sortingProcessFactory.getSortingProcessBasedOnLabel(scannedLabel));
-    var sortedCoffee = coffeeSorter.getSortingProcess().sort(truck);
-
-    assertEquals(6, sortedCoffee.size());
-    assertEquals(CoffeeBag.CoffeeQuality.DOBRA, sortedCoffee.get(1).getQuality());
-    assertEquals(CoffeeBag.CoffeeQuality.DOBRA, sortedCoffee.get(2).getQuality());
-    assertEquals(CoffeeBag.CoffeeQuality.ŚWIETNA, sortedCoffee.get(3).getQuality());
-    assertEquals(CoffeeBag.CoffeeQuality.WYBORNA, sortedCoffee.get(4).getQuality());
-
-    //pack coffee
-    PackageManager packageManager = new PackageManager(new BrazilianPackageProcess());
-    List<PackagedCoffeeBag> packagedCoffeeBags = packageManager.getPackageProcess().packCoffee(sortedCoffee);
-
+    //excpected
     PackagedCoffeeBag wyborna2kg = new PackagedCoffeeBag(CoffeeBag.CoffeeQuality.WYBORNA, 2.0);
     PackagedCoffeeBag dobra1kg = new PackagedCoffeeBag(CoffeeBag.CoffeeQuality.DOBRA, 1.0);
     PackagedCoffeeBag swietna1kg = new PackagedCoffeeBag(CoffeeBag.CoffeeQuality.ŚWIETNA, 1.0);
 
-    assertNotNull(packagedCoffeeBags);
+    //then
     assertEquals(wyborna2kg, packagedCoffeeBags.get(5));
     assertEquals(swietna1kg, packagedCoffeeBags.get(3));
     assertEquals(dobra1kg, packagedCoffeeBags.get(2));
 
+  }
 
+  private List<PackagedCoffeeBag> runProcess(List<CoffeeBag> bags, Label label) {
+    Truck truck = new Truck(label, bags);
+    LabelScanner labelScanner = new LabelScanner();
+    String scannedLabel = labelScanner.scan(truck);
+
+    ProductionLine productionLine = new ProductionLine(scannedLabel);
+    return productionLine.produceBags(truck.getBags());
   }
 
 }
