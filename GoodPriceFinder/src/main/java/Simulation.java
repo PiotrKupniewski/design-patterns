240837dce1db2f6f80ff282413c3a85bcdbfb208
p.kupniewski <=>
2021-08-26 02:09:31 +0200

    Add context

--- GoodPriceFinder/src/main/java/Simulation.java
+++ GoodPriceFinder/src/main/java/Simulation.java
@@ -1,2 +1,17 @@
 public class Simulation {
+  public static void main(String[] args) {
+
+/*
+   Mamy dwa źródła informacji:
+    - bazę danych sklepów i ich produktów
+    - wynik raportu średnich cenych na rynku dla konkretnych produktów
+   Chcemy napisać aplikację wyszukującą okazji.
+   Jak rozpocząłbyś, nie znając technicznych szczegółów źródeł danych, ani definicji "okazji", napisanie takiej aplikacji aby zasymulować jej działanie i pokazać,
+   że jest ona gotowa na różne potrzeby i technologie utzymującej dane o sklepach i raportach cen.
+   Powiedzmy, że dla pierwszej prezentacji jeżeli produkt jest w sklepie to jest on "okazją".*/
+
+    //
+    // znajdź okazję
+
+  }
 }
